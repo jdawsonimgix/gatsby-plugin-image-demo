f328cf991b38bb58aad83e709a5704ce3dc3cf59
Jamie Dawson <Jamie@imgixs-mbp.attlocal.net>
2021-10-22 16:47:29 -0700

    Added all pluggins needed to showcase gatsby-plugin-image. I also added a demo image to the index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import { StaticImage } from 'gatsby-plugin-image'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>
+    
+   <h1>Hello world!</h1>
+    <StaticImage 
+      src="../images/cat-1.jpeg"
+      width={300}
+      alt="Cat 1"
+    />
+    </div>
 }
