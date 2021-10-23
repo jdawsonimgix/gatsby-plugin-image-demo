93e84cafd5d8ace714436e7caa4b8abfd2f9b5c2
Jamie Dawson <Jamie@imgixs-mbp.attlocal.net>
2021-10-22 17:07:17 -0700

    updated images for display. Also updated image formats

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,147 @@ import { StaticImage } from 'gatsby-plugin-image'
 export default function Home() {
   return <div>
     
-   <h1>Hello world!</h1>
+   <h1>Demo app using gatsby-plugin-image</h1>
+
     <StaticImage 
       src="../images/cat-1.jpeg"
       width={300}
       alt="Cat 1"
+      formats={["AUTO", "WEBP", "AVIF"]}
     />
+
+<StaticImage 
+      src="../images/cat-1.jpeg"
+      width={300}
+      alt="Cat 1"
+      formats={["AUTO", "WEBP", "AVIF"]}
+    />
+
+<StaticImage 
+      src="../images/cat-1.jpeg"
+      width={300}
+      alt="Cat 1"
+      formats={["AUTO", "WEBP", "AVIF"]}
+    />
+
+<StaticImage 
+      src="../images/cat-1.jpeg"
+      width={300}
+      alt="Cat 1"
+      formats={["AUTO", "WEBP", "AVIF"]}
+    />
+
+<StaticImage 
+      src="../images/cat-1.jpeg"
+      width={300}
+      alt="Cat 1"
+      formats={["AUTO", "WEBP", "AVIF"]}
+    />
+
+<StaticImage 
+      src="../images/cat-1.jpeg"
+      width={300}
+      alt="Cat 1"
+      formats={["AUTO", "WEBP", "AVIF"]}
+    />
+
+<StaticImage 
+      src="../images/cat-1.jpeg"
+      width={300}
+      alt="Cat 1"
+      formats={["AUTO", "WEBP", "AVIF"]}
+    />
+
+<StaticImage 
+      src="../images/cat-1.jpeg"
+      width={300}
+      alt="Cat 1"
+      formats={["AUTO", "WEBP", "AVIF"]}
+    />
+
+<StaticImage 
+      src="../images/cat-1.jpeg"
+      width={300}
+      alt="Cat 1"
+      formats={["AUTO", "WEBP", "AVIF"]}
+    />
+
+<StaticImage 
+      src="../images/cat-1.jpeg"
+      width={300}
+      alt="Cat 1"
+      formats={["AUTO", "WEBP", "AVIF"]}
+    />
+
+<StaticImage 
+      src="../images/cat-1.jpeg"
+      width={300}
+      alt="Cat 1"
+      formats={["AUTO", "WEBP", "AVIF"]}
+    />
+
+<StaticImage 
+      src="../images/cat-1.jpeg"
+      width={300}
+      alt="Cat 1"
+      formats={["AUTO", "WEBP", "AVIF"]}
+    />
+
+<StaticImage 
+      src="../images/cat-1.jpeg"
+      width={300}
+      alt="Cat 1"
+      formats={["AUTO", "WEBP", "AVIF"]}
+    />
+
+<StaticImage 
+      src="../images/cat-1.jpeg"
+      width={300}
+      alt="Cat 1"
+      formats={["AUTO", "WEBP", "AVIF"]}
+    />
+
+<StaticImage 
+      src="../images/cat-1.jpeg"
+      width={300}
+      alt="Cat 1"
+      formats={["AUTO", "WEBP", "AVIF"]}
+    />
+
+<StaticImage 
+      src="../images/cat-1.jpeg"
+      width={300}
+      alt="Cat 1"
+      formats={["AUTO", "WEBP", "AVIF"]}
+    />
+
+<StaticImage 
+      src="../images/cat-1.jpeg"
+      width={300}
+      alt="Cat 1"
+      formats={["AUTO", "WEBP", "AVIF"]}
+    />
+
+<StaticImage 
+      src="../images/cat-1.jpeg"
+      width={300}
+      alt="Cat 1"
+      formats={["AUTO", "WEBP", "AVIF"]}
+    />
+
+<StaticImage 
+      src="../images/cat-1.jpeg"
+      width={300}
+      alt="Cat 1"
+      formats={["AUTO", "WEBP", "AVIF"]}
+    />
+
+<StaticImage 
+      src="../images/cat-1.jpeg"
+      width={300}
+      alt="Cat 1"
+      formats={["AUTO", "WEBP", "AVIF"]}
+    />
+
     </div>
 }
